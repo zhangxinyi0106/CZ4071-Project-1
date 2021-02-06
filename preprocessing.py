1983aeacc42bd49884bc3de8c7ce2c124141e560
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2021-02-06 11:38:57 +0800

    (WIP) construct graph

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,12 @@
 import os.path as osp
-import re
-import json
 import pickle
+import re
 
+import networkx as nx
+import pandas as pd
 import requests
 import xmltodict
 from tqdm import tqdm
-import pandas as pd
 
 from data import DATA_PATH
 
@@ -18,7 +18,7 @@ def _read_xlsx_file(path, filename, sheet_name, required_fields: set):
         raise FileNotFoundError("the appointed file {} does not exist!".format(target_file))
 
     name_list = pd.read_excel(target_file, sheet_name=sheet_name)
-    assert set(name_list.columns.values).issuperset(required_fields),\
+    assert set(name_list.columns.values).issuperset(required_fields), \
         'required fields are missing from the sheet!'
     return name_list
 
@@ -47,13 +47,13 @@ def read_top_conferences(path=DATA_PATH, filename='Top.xlsx', sheet_name='Sheet1
                            required_fields={'Area', 'Venue', 'Comments'})
 
 
-def fetch_dblp_profile(auth_name_list, reuse=False, target_pickle_name=None):
+def fetch_dblp_profile(auth_name_data, reuse=False, target_pickle_name=None):
     """
     Fetch dblp personal profiles given a name list
-    :param auth_name_list: a name list containing urls
+    :param auth_name_data: a name list containing urls with faculty information
     :param reuse: True if to read from pickle directly
     :param target_pickle_name: target pickle, used when reuse == True
-    :return: list of dict
+    :return: dictionary with name as key
     """
     if reuse:
         assert target_pickle_name is not None, 'Please specify a pickle to use'
@@ -63,16 +63,19 @@ def fetch_dblp_profile(auth_name_list, reuse=False, target_pickle_name=None):
         except FileNotFoundError:
             print("Target pickle not found! Re-retrieving data...")
 
-    url_list = list(auth_name_list['DBLP'])
-    profile_list = []
+    url_list = list(auth_name_data['DBLP'])
+    name_list = list(auth_name_data['Faculty'])
+    assert len(set(name_list)) == len(name_list), 'Duplicated names found!'
+
+    profile_data = dict()
     with tqdm(total=len(url_list)) as pbar:
-        for url in url_list:
+        for i, url in enumerate(url_list):
             # noinspection PyBroadException
             try:
                 true_url = requests.get(url).url  # sometimes .xml will be converted to .html after redirection
                 true_url = re.sub(r'(?<=\.)html$', 'xml', true_url)
                 profile_in_xml = requests.get(true_url).content
-                profile_list.append(xmltodict.parse(profile_in_xml, dict_constructor=dict))
+                profile_data[name_list[i]] = xmltodict.parse(profile_in_xml, dict_constructor=dict)
                 pbar.update(1)
             except:
                 print(f'url {url} not fetched!')
@@ -80,16 +83,37 @@ def fetch_dblp_profile(auth_name_list, reuse=False, target_pickle_name=None):
 
     with open(osp.join(DATA_PATH, f'{target_pickle_name if target_pickle_name is not None else "profiles"}.pickle'),
               'wb') as f:
-        pickle.dump(profile_list, f)
+        pickle.dump(profile_data, f)
 
-    return profile_list
+    return profile_data
 
 
-def generate_graph(year=None):
-    pass
+def generate_graph(name_data: pd.DataFrame, profile_data: dict, till_year=None, faculty_member_only=True):
+    """
+    construct a graph from the given faculty list and dblp data
+    :param name_data:
+    :param profile_data:
+    :param till_year: (included) data till witch year that the graph should present
+    :param faculty_member_only: True if excluding all other non-SCSE co-authors
+    :return: graph
+    """
+    # TODO: add support for 'by_year'
+    if not faculty_member_only or till_year is not None:
+        raise NotImplementedError
+
+    graph = nx.Graph()
+
+    # add nodes
+    for name in name_data.Faculty.unique():
+        properties = name_data.loc[name_data['Faculty'] == name].drop("Faculty", 1).squeeze().to_dict()
+        graph.add_node(name, **properties)
+
+    # TODO: add edges
+    for k, v in profile_data.items():
+        pass
 
 
 if __name__ == '__main__':
-    auth_name_list = read_faculty()
-    auth_profiles = fetch_dblp_profile(auth_name_list=auth_name_list, reuse=True, target_pickle_name='profiles')
-
+    auth_name_data = read_faculty()
+    auth_profiles = fetch_dblp_profile(auth_name_data=auth_name_data, reuse=True, target_pickle_name='profiles')
+    generate_graph(auth_name_data, auth_profiles)
