c52bd537d99b5f20210b45deb281291751794e8d
VInci-Hu <huwenqi614@gmail.com>
2021-03-15 22:32:01 +0800

    Combine Util functions into Analyzer class
    with test on real data

--- faculty.py
+++ faculty.py
@@ -1,7 +1,9 @@
 from typing import Set
 
 from preprocessing import *
-
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
 
 class Analyzer:
     venue_to_booktitle = dict({
@@ -87,6 +89,19 @@ class Analyzer:
                         excellence[k] += 1
         return excellence
 
+    def _get_names_in_rank(cls, rank_str: str) -> set:
+        """
+        return the names in a particular rank(position)
+        :return: a set of faculty names
+        """
+        names = set()
+        for index, row in cls.auth_name_data.iterrows():
+            if row['Position'] == rank_str:
+                names.add(row['Faculty'])
+        if len(names) == 0:
+            raise ValueError(f'Unexpected Rank Name {rank_str} Encountered!')
+        return names
+
     @classmethod
     def filter_graph_by_names(cls, source_graphs: Union[nx.Graph, List[nx.Graph]],
                               faculty_names: Union[Set[str], List[str]]) -> Union[nx.Graph, List[nx.Graph]]:
@@ -104,6 +119,24 @@ class Analyzer:
         else:
             return cls._get_subgraph(source_graphs, faculty_names)
 
+    def filter_graph_by_rank(self, source_graphs: Union[nx.Graph, List[nx.Graph]],
+                              ranks: Union[Set[str], List[str]]) -> Union[nx.Graph, List[nx.Graph]]:
+        """
+        Get subgraph(s) that filter the nodes with the designated ranks
+        :param source_graphs: the original complete graph(s)
+        :param ranks: list of the faculty ranks, e.g.["Professor", "Assistant Professor"]
+        :return: networkx graph or list of graphs, depending on the number or source graph passed
+        """
+        if type(ranks) is not set:
+            ranks = set(ranks)
+        faculty_names = set()
+        for rank in ranks:
+            faculty_names.update(self._get_names_in_rank(rank_str=rank))
+        if type(source_graphs) is list:
+            return [self._get_subgraph(source_graph=g, node_names=faculty_names) for g in source_graphs]
+        else:
+            return self._get_subgraph(source_graphs, faculty_names)
+
     @staticmethod
     def _get_subgraph(source_graph: nx.Graph, node_names: Set[str]) -> nx.Graph:
         unexpected_names = node_names - set(source_graph.nodes)
@@ -111,9 +144,92 @@ class Analyzer:
             print(f"Unexpected Name(s) {unexpected_names} Encountered: No Such Node(s) in the Source Graph")
         return nx.subgraph(source_graph, node_names)
 
+    @staticmethod
+    def get_avg_degree(graph):
+        """
+        Return the average degree of a Networkx graph.
+        For other basic information, call nx functions directly.
+        e.g.  G.number_of_nodes(); G.number_of_edges(); nx.density(G)
+        :param graph: a networkx graph object
+        :return: a float number
+        """
+        nnodes = graph.number_of_nodes()
+        s = sum(dict(graph.degree()).values())
+        return float(s) / float(nnodes)
+
+    @staticmethod
+    def get_largest_component_diameter(graph):
+        """
+        Return the diameter of the largest component of the graph, prevents unconnected exception.
+        :param graph: a networkx graph object
+        :return: an integer
+        """
+        # to get the list of components
+        components = nx.connected_components(graph)
+        # use the max() command to find the largest one:
+        largest_component = max(components, key=len)
+        # construct a subgraph of largest component
+        subgraph = G.subgraph(largest_component)
+        diameter = nx.diameter(subgraph)
+        return diameter
+
+    @staticmethod
+    def get_clustering_coeff(graph):
+        """
+        Return the clustering coefficient of the graph
+        :param graph: a networkx graph object
+        :return: a float number
+        """
+        clust_coeff = nx.average_clustering(graph)
+        return clust_coeff
+
+    @staticmethod
+    def plot_degree_distribution_hist(G):
+        """
+        Plot the degree distribution histogram for the graph.
+        :param graph: a networkx graph object
+        """
+        degrees = [G.degree(n) for n in G.nodes()]
+        plt.hist(degrees)
+        plt.xlabel("k")
+        plt.ylabel("Hist")
+        plt.title("Degree distribution_Histogram")
+        plt.show()
+
+    @staticmethod
+    def plot_degree_distribution_loglog(G, normalized=True):
+        """
+        Plot the log-log degree distribution for the graph.
+        In this case, number of nodes are too small to show a beautiful plot.
+        :param graph: a networkx graph object
+        """
+        aux_y = nx.degree_histogram(G)
+        aux_x = np.arange(0, len(aux_y)).tolist()
+        n_nodes = G.number_of_nodes()
+        if normalized:
+            for i in range(len(aux_y)):
+                aux_y[i] = aux_y[i] / n_nodes
+        plt.loglog(aux_x, aux_y)
+        plt.xlabel("k")
+        plt.ylabel("P(k)")
+        plt.title("Degree distribution_Loglog")
+        plt.show()
+
+    @staticmethod
+    def analyze_centrality_of_sub_graph(subgraph):
+        """
+        Unfinished implementation.
+        :param subgraph:
+        :return:
+        """
+        basic_info = subgraph.info()
+        print(basic_info)
+
 
 if __name__ == '__main__':
     analyzer = Analyzer()
     T, G = generate_graphs(name_data=analyzer.auth_name_data, profile_data=analyzer.auth_profiles)
-    subgraphs = analyzer.filter_graph_by_names(G, ['Miao Chunyan', 'Tan Rui', 'Wen Yonggang', 'AAAA'])
+    # analyzer.plot_degree_distribution_hist(G[5])
+    # subgraphs = analyzer.filter_graph_by_names(G, ['Miao Chunyan', 'Tan Rui', 'Wen Yonggang', 'AAAA'])
+    subgraphs = analyzer.filter_graph_by_rank(G, {'Professor','Lecturer'})
     visualize_graphs(tags=T, graphs=subgraphs, port=get_free_port())
